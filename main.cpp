00327c7c3bb3124e1ae29e8bba9183977e06c894
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2024-05-01 11:32:20 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
-#include<iostream>
-using namespace std;
-
+#include "raylib.h"
+#include "raymath.h"
+#include "character.h"
 
 int main()
 {
+    const int windowDimensions[] = {384, 384};
 
+    InitWindow(windowDimensions[0], windowDimensions[1], "Top down game");
 
+    Texture2D map = LoadTexture("nature_tileset/world map.png");
+    Vector2 mapPos = {0.0, 0.0};
+    const float mapScale{4.0f};
 
+    Character knight(windowDimensions[0], windowDimensions[1]);
 
-return 0;
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+        mapPos = Vector2Scale(knight.getWorldpos(), -1.f);
+
+        // Draw the map
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+        knight.tick(GetFrameTime());
+
+        if (knight.getWorldpos().x < 0.f ||
+            knight.getWorldpos().y < 0.f ||
+            (knight.getWorldpos().x + windowDimensions[0]) > map.width *mapScale ||
+            (knight.getWorldpos().y + windowDimensions[1]) > map.height *mapScale)
+            {
+                knight.undoMovemment();
+            }
+
+            EndDrawing();
+    }
+
+    UnloadTexture(map);
+    CloseWindow();
+    return 0;
 }
