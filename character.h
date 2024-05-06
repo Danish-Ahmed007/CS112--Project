ea1e00b896d9a546b0ec0b30918ce78298fbe979
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2024-05-06 09:08:04 +0500

    Update character.h

--- character.h
+++ character.h
@@ -1,1 +1,32 @@
+#include "raylib.h"
 
+class Character
+
+{
+public:
+    Vector2 getWorldpos() { return worldPos; }
+    void tick(float deltaTime);
+    void undoMovemment();
+    Character(int windowWidth, int windowHeight);
+    Rectangle GetCollisionRec();
+
+private:
+    Texture2D texture = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D textureIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D textureRun = LoadTexture("characters/knight_run_spritesheet.png");
+    Vector2 screenPos{};
+    Vector2 worldPos{};
+    Vector2 worldPosLastFrame{};
+    // 1. facing right direction -1. facing left direction
+    float rightLeft{1.f};
+
+    // knght variables
+    float runningTime{};
+    int frame{};
+    int maxFrame{6};
+    float updateTime{1.f / 12.f};
+    const float speed{4.0};
+    float width;
+    float height;
+    float scale{4.f};
+};
