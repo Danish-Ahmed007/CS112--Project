946143ccc48cdb1794cf2829e43f163faa2bf02b
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2024-05-13 12:58:39 +0500

    Update prop.h

--- prop.h
+++ prop.h
@@ -2,13 +2,12 @@
 
 class Prop
 {
-
 public:
-    Prop(Texture2D tex, Vector2 pos);
+    Prop(Vector2 pos, Texture2D tex);
     void Render(Vector2 knightPos);
-
+    Rectangle getCollisionRec(Vector2 knightPos);
 private:
     Texture2D texture{};
     Vector2 worldPos{};
     float scale{4.f};
-}; 
+};
