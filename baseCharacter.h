70df27d7c6c38cc08112bc9f9aab968996350f0a
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2024-05-13 12:53:52 +0500

    Update baseCharacter.h

--- baseCharacter.h
+++ baseCharacter.h
@@ -1,7 +1,3 @@
-
-
-
-
 #ifndef BASE_CHARACTER_H
 #define BASE_CHARACTER_H
 #include "raylib.h"
@@ -12,11 +8,14 @@ public:
     Vector2 getWorldPos() { return worldPos; }
     void undoMovement();
     Rectangle getCollisionRec();
+    virtual void tick(float deltaTime);
+    virtual Vector2 getScreenPos() =0;
+    bool getAlive(){return alive;}
+    void setAlive(bool isAlive){alive = isAlive;}
 protected:
     Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
     Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
     Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    Vector2 screenPos{};
     Vector2 worldPos{};
     Vector2 worldPosLastFrame{};
     // 1 : facing right, -1 : facing left
@@ -30,9 +29,12 @@ protected:
     float width{};
     float height{};
     float scale{4.0f};
-
+    Vector2 velocity{};
 private:
-
+    bool alive{true};
 };
 
 #endif
+
+
+
