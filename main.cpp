c413146e7f9ce2a504f077ec76611ba0a831b824
Danish-Ahmed007 <165469250+Danish-Ahmed007@users.noreply.github.com>
2024-05-13 12:57:35 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,123 @@
 #include "raylib.h"
 #include "raymath.h"
-#include "character.h"
+#include "Character.h"
+#include "Prop.h"
+#include "Enemy.h"
+#include <string>
 
 int main()
 {
-    const int windowDimensions[] = {384, 384};
+    const int windowWidth{384};
+    const int windowHeight{384};
+    InitWindow(windowWidth, windowHeight, "Danish Classy Clash Game");
 
-    InitWindow(windowDimensions[0], windowDimensions[1], "Top down game");
-
-    Texture2D map = LoadTexture("nature_tileset/world map.png");
-    Vector2 mapPos = {0.0, 0.0};
+    Texture2D map = LoadTexture("nature_tileset/worldMap.png");
+    Vector2 mapPos{0.0, 0.0};
     const float mapScale{4.0f};
 
-    Character knight(windowDimensions[0], windowDimensions[1]);
+    Character knight{windowWidth, windowHeight};
+
+    Prop props[]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{800.f, 700.f}, LoadTexture("nature_tileset/Log.png")},
+        Prop{Vector2{700.f, 1000.f}, LoadTexture("nature_tileset/Sign.png")},
+        Prop{Vector2{1000.f, 1400.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{900.f, 1100.f}, LoadTexture("nature_tileset/Bush.png")},
+        Prop{Vector2{450.f, 500.f}, LoadTexture("nature_tileset/Bush.png")}
+        };
+
+    Enemy goblin{
+        Vector2{500, 800},
+        LoadTexture("characters/goblin_idle_spritesheet.png"),
+        LoadTexture("characters/goblin_run_spritesheet.png")};
+    Enemy goblin2{
+        Vector2{1000, 2500},
+        LoadTexture("characters/goblin_idle_spritesheet.png"),
+        LoadTexture("characters/goblin_run_spritesheet.png")};
+
+    Enemy slime{
+        Vector2{800, 500},
+        LoadTexture("characters/slime_idle_spritesheet.png"),
+        LoadTexture("characters/slime_run_spritesheet.png")};
+    Enemy slime2{
+        Vector2{1500, 1000},
+        LoadTexture("characters/slime_idle_spritesheet.png"),
+        LoadTexture("characters/slime_run_spritesheet.png")};
+
+    Enemy *enemies[] = {
+        &goblin, &slime, &slime2, &goblin2};
+
+    for (auto enemy : enemies)
+    {
+        enemy->setTarget(&knight);
+    }
 
     SetTargetFPS(60);
-
     while (!WindowShouldClose())
     {
-
         BeginDrawing();
         ClearBackground(WHITE);
 
-        mapPos = Vector2Scale(knight.getWorldpos(), -1.f);
+        mapPos = Vector2Scale(knight.getWorldPos(), -1.f);
 
-        // Draw the map
-        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
-        knight.tick(GetFrameTime());
+        // draw the map
+        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
 
-        if (knight.getWorldpos().x < 0.f ||
-            knight.getWorldpos().y < 0.f ||
-            (knight.getWorldpos().x + windowDimensions[0]) > map.width *mapScale ||
-            (knight.getWorldpos().y + windowDimensions[1]) > map.height *mapScale)
-            {
-                knight.undoMovemment();
-            }
+        // draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
 
+        if (!knight.getAlive()) // character is not alive
+        {
+            DrawText("Game Over", 100.f, 180.f, 40, RED);
             EndDrawing();
-    }
+            continue;
+        }
+        else
+        {
+            std::string knightHealth = "Health: ";
+            knightHealth.append(std::to_string(knight.gethealth()), 0, 5);
+            DrawText(knightHealth.c_str(), 55.f, 45.f, 40, RED);
+        }
 
-    UnloadTexture(map);
+        knight.tick(GetFrameTime());
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > map.width * mapScale ||
+            knight.getWorldPos().y + windowHeight > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+        // check prop collisions
+        for (auto prop : props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
+
+        for (auto enemy : enemies)
+        {
+            enemy->tick(GetFrameTime());
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) == true)
+        {
+            for (auto enemy : enemies)
+            {
+
+                if (CheckCollisionRecs(enemy->getCollisionRec(), knight.getWeaponCollisonRec()) == true)
+                {
+                    enemy->setAlive(false);
+                }
+            }
+        }
+
+        EndDrawing();
+    }
     CloseWindow();
-    return 0;
 }
